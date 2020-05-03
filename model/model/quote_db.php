6e1e02107ceab5fd4f5a3e9eb4420267c71060d4
ericwstevenson <42499776+ericwstevenson@users.noreply.github.com>
2020-05-03 18:42:17 -0500

    Add files via upload

---
+++ model/model/quote_db.php
@@ -0,0 +1,55 @@
+<?php 
+    function get_all_quotes($sort) {
+        global $db;
+        if ($sort == 'author'){
+            $orderby = 'Q.category';
+        } else {
+            $orderby = 'Q.author';
+        }
+        $query = 'SELECT Q.text, Q.author, Q.category 
+            FROM quotes Q
+            LEFT JOIN authors A ON Q.Author_code = A.Code 
+            LEFT JOIN categories C ON Q.Category_code = C.Code 
+            ORDER BY ' . $orderby . ' DESC';
+        $statement = $db->prepare($query);
+        $statement->execute();
+        $quotes = $statement->fetchAll();
+        $statement->closeCursor();
+        return $quotes;
+    }
+
+    function get_quote($quote_id) {
+        global $db;
+        $query = 'SELECT * FROM quotes WHERE IndexNum = :quote_id';
+        $statement = $db->prepare($query);
+        $statement->bindValue(':quote_id', $quote_id);
+        $statement->execute();
+        $quote = $statement->fetch();
+        $statement->closeCursor();
+        return $quote;
+    }
+
+    function delete_quote($quote_id) {
+        global $db;
+        $query = 'DELETE FROM quotes WHERE IndexNum = :quote_id';
+        $statement = $db->prepare($query);
+        $statement->bindValue(':quote_id', $quote_id);
+        $statement->execute();
+        $statement->closeCursor();
+    }
+
+    function add_quote($text, $category_id, $author_id) {
+        global $db;
+        $query = 'INSERT INTO quotes (Text, Author, Category, Category_code, Author_code, IndexNum)
+              VALUES
+                 (:text, :author, :category, :category_id, :author_id, IndexNum';
+        $statement = $db->prepare($query);
+        $statement->bindValue(':text', $text);
+        $statement->bindValue(':author', $author);
+        $statement->bindValue(':category', $category);
+        $statement->bindValue(':author', $author_id);
+        $statement->bindValue(':category', $category_id);
+        $statement->execute();
+        $statement->closeCursor();
+    }
+?>
